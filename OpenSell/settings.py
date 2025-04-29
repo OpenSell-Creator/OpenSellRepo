12c674350275cff00dc7b12f8bfd8f77cb7b9cef
Open-Sell <opensellmarketplace@gmail.com>
2025-04-29 13:31:43 +0100

    Update settings.py
    added few security lines

--- OpenSell/settings.py
+++ OpenSell/settings.py
@@ -33,7 +33,17 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
-
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    
+    
 # Application definition
 
 INSTALLED_APPS = [
