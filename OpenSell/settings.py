c5f81a1153d7c295b7904d59e43181458149040a
Open-Sell <opensellmarketplace@gmail.com>
2025-05-14 21:42:51 +0100

    Update settings.py
    quick settings update

--- OpenSell/settings.py
+++ OpenSell/settings.py
@@ -296,8 +296,11 @@ if not DEBUG:
     EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    
     EMAIL_USE_TLS = True
-    DEFAULT_FROM_EMAIL = 'OpenSell'
+    EMAIL_USE_SSL = False
+    
+    DEFAULT_FROM_EMAIL = 'OpenSell <support@opensell.online>'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
