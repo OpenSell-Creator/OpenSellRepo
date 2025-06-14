4bdd56052ea08ff90a4b0fb723deece4dcf100a3
Open-Sell <opensellmarketplace@gmail.com>
2025-06-14 13:25:10 +0100

    Update states_and_lgas.py

--- User/management/commands/states_and_lgas.py
+++ User/management/commands/states_and_lgas.py
@@ -2,10 +2,11 @@ from django.core.management.base import BaseCommand
 from User.models import State, LGA
 
 class Command(BaseCommand):
-    help = 'Load or update all South West Nigerian States and their LGAs'
+    help = 'Load or update all Nigerian States and their LGAs'
 
     def handle(self, *args, **kwargs):
-        southwest_data = {
+        nigerian_states_data = {
+            # SOUTH WEST
             "Ekiti": [
                 "Ado Ekiti", "Efon", "Ekiti East", "Ekiti South-West", "Ekiti West", "Emure",
                 "Gbonyin", "Ido/Osi", "Ijero", "Ikere", "Ikole", "Ilejemeje", "Irepodun/Ifelodun",
@@ -42,19 +43,199 @@ class Command(BaseCommand):
                 "Kajola", "Lagelu", "Ogbomosho North", "Ogbomosho South", "Ogo Oluwa", "Olorunsogo",
                 "Oluyole", "Ona Ara", "Orelope", "Oriire", "Oyo East", "Oyo West", "Saki East",
                 "Saki West", "Surulere"
+            ],
+
+            # SOUTH SOUTH
+            "Akwa Ibom": [
+                "Abak", "Eastern Obolo", "Eket", "Esit Eket", "Essien Udim", "Etim Ekpo", "Etinan",
+                "Ibeno", "Ibesikpo Asutan", "Ibiono-Ibom", "Ika", "Ikono", "Ikot Abasi", "Ikot Ekpene",
+                "Ini", "Itu", "Mbo", "Mkpat-Enin", "Nsit-Atai", "Nsit-Ibom", "Nsit-Ubium", "Obot Akara",
+                "Okobo", "Onna", "Oron", "Oruk Anam", "Udung-Uko", "Ukanafun", "Uruan", "Urue-Offong/Oruko", "Uyo"
+            ],
+            "Bayelsa": [
+                "Brass", "Ekeremor", "Kolokuma/Opokuma", "Nembe", "Ogbia", "Sagbama", "Southern Ijaw", "Yenagoa"
+            ],
+            "Cross River": [
+                "Abi", "Akamkpa", "Akpabuyo", "Bakassi", "Bekwarra", "Biase", "Boki", "Calabar Municipal",
+                "Calabar South", "Etung", "Ikom", "Obanliku", "Obubra", "Obudu", "Odukpani", "Ogoja",
+                "Yakuur", "Yala"
+            ],
+            "Delta": [
+                "Aniocha North", "Aniocha South", "Bomadi", "Burutu", "Ethiope East", "Ethiope West",
+                "Ika North East", "Ika South", "Isoko North", "Isoko South", "Ndokwa East", "Ndokwa West",
+                "Okpe", "Oshimili North", "Oshimili South", "Patani", "Sapele", "Udu", "Ughelli North",
+                "Ughelli South", "Ukwuani", "Uvwie", "Warri North", "Warri South", "Warri South West"
+            ],
+            "Edo": [
+                "Akoko-Edo", "Egor", "Esan Central", "Esan North-East", "Esan South-East", "Esan West",
+                "Etsako Central", "Etsako East", "Etsako West", "Igueben", "Ikpoba Okha", "Oredo",
+                "Orhionmwon", "Ovia North-East", "Ovia South-West", "Owan East", "Owan West", "Uhunmwonde"
+            ],
+            "Rivers": [
+                "Abua/Odual", "Ahoada East", "Ahoada West", "Akuku-Toru", "Andoni", "Asari-Toru", "Bonny",
+                "Degema", "Eleme", "Emohua", "Etche", "Gokana", "Ikwerre", "Khana", "Obio/Akpor", "Ogba/Egbema/Ndoni",
+                "Ogu/Bolo", "Okrika", "Omuma", "Opobo/Nkoro", "Oyigbo", "Port Harcourt", "Tai"
+            ],
+
+            # SOUTH EAST
+            "Abia": [
+                "Aba North", "Aba South", "Arochukwu", "Bende", "Ikwuano", "Isiala Ngwa North",
+                "Isiala Ngwa South", "Isuikwuato", "Obi Ngwa", "Ohafia", "Osisioma", "Ugwunagbo",
+                "Ukwa East", "Ukwa West", "Umuahia North", "Umuahia South", "Umu Nneochi"
+            ],
+            "Anambra": [
+                "Aguata", "Anambra East", "Anambra West", "Anaocha", "Awka North", "Awka South",
+                "Ayamelum", "Dunukofia", "Ekwusigo", "Idemili North", "Idemili South", "Ihiala",
+                "Njikoka", "Nnewi North", "Nnewi South", "Ogbaru", "Onitsha North", "Onitsha South",
+                "Orumba North", "Orumba South", "Oyi"
+            ],
+            "Ebonyi": [
+                "Abakaliki", "Afikpo North", "Afikpo South", "Ebonyi", "Ezza North", "Ezza South",
+                "Ikwo", "Ishielu", "Ivo", "Izzi", "Ohaozara", "Ohaukwu", "Onicha"
+            ],
+            "Enugu": [
+                "Aninri", "Awgu", "Enugu East", "Enugu North", "Enugu South", "Ezeagu", "Igbo Etiti",
+                "Igbo Eze North", "Igbo Eze South", "Isi Uzo", "Nkanu East", "Nkanu West", "Nsukka",
+                "Oji River", "Udenu", "Udi", "Uzo Uwani"
+            ],
+            "Imo": [
+                "Aboh Mbaise", "Ahiazu Mbaise", "Ehime Mbano", "Ezinihitte", "Ideato North", "Ideato South",
+                "Ihitte/Uboma", "Ikeduru", "Isiala Mbano", "Isu", "Mbaitoli", "Ngor Okpala", "Njaba",
+                "Nkwerre", "Nwangele", "Obowo", "Oguta", "Ohaji/Egbema", "Okigwe", "Orlu", "Orsu",
+                "Oru East", "Oru West", "Owerri Municipal", "Owerri North", "Owerri West", "Unuimo"
+            ],
+
+            # NORTH CENTRAL
+            "Benue": [
+                "Ado", "Agatu", "Apa", "Buruku", "Gboko", "Guma", "Gwer East", "Gwer West", "Katsina-Ala",
+                "Konshisha", "Kwande", "Logo", "Makurdi", "Obi", "Ogbadibo", "Ohimini", "Oju", "Okpokwu",
+                "Otukpo", "Tarka", "Ukum", "Ushongo", "Vandeikya"
+            ],
+            "Kogi": [
+                "Adavi", "Ajaokuta", "Ankpa", "Bassa", "Dekina", "Ibaji", "Idah", "Igalamela Odolu",
+                "Ijumu", "Kabba/Bunu", "Kogi", "Lokoja", "Mopa Muro", "Ofu", "Ogori/Magongo", "Okehi",
+                "Okene", "Olamaboro", "Omala", "Yagba East", "Yagba West"
+            ],
+            "Kwara": [
+                "Asa", "Baruten", "Edu", "Ekiti", "Ifelodun", "Ilorin East", "Ilorin South", "Ilorin West",
+                "Irepodun", "Isin", "Kaiama", "Moro", "Offa", "Oke Ero", "Oyun", "Pategi"
+            ],
+            "Nasarawa": [
+                "Akwanga", "Awe", "Doma", "Karu", "Keana", "Keffi", "Kokona", "Lafia", "Nasarawa",
+                "Nasarawa Egon", "Obi", "Toto", "Wamba"
+            ],
+            "Niger": [
+                "Agaie", "Agwara", "Bida", "Borgu", "Bosso", "Chanchaga", "Edati", "Gbako", "Gurara",
+                "Katcha", "Kontagora", "Lapai", "Lavun", "Magama", "Mariga", "Mashegu", "Mokwa", "Munya",
+                "Paikoro", "Rafi", "Rijau", "Shiroro", "Suleja", "Tafa", "Wushishi"
+            ],
+            "Plateau": [
+                "Barkin Ladi", "Bassa", "Bokkos", "Jos East", "Jos North", "Jos South", "Kanam", "Kanke",
+                "Langtang North", "Langtang South", "Mangu", "Mikang", "Pankshin", "Qua'an Pan", "Riyom",
+                "Shendam", "Wase"
+            ],
+            "Federal Capital Territory": [
+                "Abaji", "Bwari", "Gwagwalada", "Kuje", "Municipal Area Council", "Kwali"
+            ],
+
+            # NORTH EAST
+            "Adamawa": [
+                "Demsa", "Fufure", "Ganye", "Gayuk", "Gombi", "Grie", "Hong", "Jada", "Larmurde",
+                "Madagali", "Maiha", "Mayo Belwa", "Michika", "Mubi North", "Mubi South", "Numan",
+                "Shelleng", "Song", "Toungo", "Yola North", "Yola South"
+            ],
+            "Bauchi": [
+                "Alkaleri", "Bauchi", "Bogoro", "Damban", "Darazo", "Dass", "Gamawa", "Ganjuwa",
+                "Giade", "Itas/Gadau", "Jama'are", "Katagum", "Kirfi", "Misau", "Ningi", "Shira",
+                "Tafawa Balewa", "Toro", "Warji", "Zaki"
+            ],
+            "Borno": [
+                "Abadam", "Askira/Uba", "Bama", "Bayo", "Biu", "Chibok", "Damboa", "Dikwa", "Gubio",
+                "Guzamala", "Gwoza", "Hawul", "Jere", "Kaga", "Kala/Balge", "Konduga", "Kukawa",
+                "Kwaya Kusar", "Mafa", "Magumeri", "Maiduguri", "Marte", "Mobbar", "Monguno", "Ngala",
+                "Nganzai", "Shani"
+            ],
+            "Gombe": [
+                "Akko", "Balanga", "Billiri", "Dukku", "Funakaye", "Gombe", "Kaltungo", "Kwami",
+                "Nafada", "Shongom", "Yamaltu/Deba"
+            ],
+            "Taraba": [
+                "Ardo Kola", "Bali", "Donga", "Gashaka", "Gassol", "Ibi", "Jalingo", "Karim Lamido",
+                "Kurmi", "Lau", "Sardauna", "Takum", "Ussa", "Wukari", "Yorro", "Zing"
+            ],
+            "Yobe": [
+                "Barde", "Bursari", "Damaturu", "Fika", "Fune", "Geidam", "Gujba", "Gulani", "Jakusko",
+                "Karasuwa", "Machina", "Nangere", "Nguru", "Potiskum", "Tarmuwa", "Yunusari", "Yusufari"
+            ],
+
+            # NORTH WEST
+            "Jigawa": [
+                "Auyo", "Babura", "Biriniwa", "Birnin Kudu", "Buji", "Dutse", "Gagarawa", "Garki",
+                "Gumel", "Guri", "Gwaram", "Gwiwa", "Hadejia", "Jahun", "Kafin Hausa", "Kaugama",
+                "Kazaure", "Kiri Kasama", "Kiyawa", "Maigatari", "Malam Madori", "Miga", "Ringim",
+                "Roni", "Sule Tankarkar", "Taura", "Yankwashi"
+            ],
+            "Kaduna": [
+                "Birnin Gwari", "Chikun", "Giwa", "Igabi", "Ikara", "Jaba", "Jema'a", "Kachia", "Kaduna North",
+                "Kaduna South", "Kagarko", "Kajuru", "Kaura", "Kauru", "Kubau", "Kudan", "Lere", "Makarfi",
+                "Sabon Gari", "Sanga", "Soba", "Zangon Kataf", "Zaria"
+            ],
+            "Kano": [
+                "Ajingi", "Albasu", "Bagwai", "Bebeji", "Bichi", "Bunkure", "Dala", "Dambatta", "Dawakin Kudu",
+                "Dawakin Tofa", "Doguwa", "Fagge", "Gabasawa", "Garko", "Garun Mallam", "Gaya", "Gezawa",
+                "Gwale", "Gwarzo", "Kabo", "Kano Municipal", "Karaye", "Kibiya", "Kiru", "Kumbotso", "Kunchi",
+                "Kura", "Madobi", "Makoda", "Minjibir", "Nasarawa", "Rano", "Rimin Gado", "Rogo", "Shanono",
+                "Sumaila", "Takai", "Tarauni", "Tofa", "Tsanyawa", "Tudun Wada", "Ungogo", "Warawa", "Wudil"
+            ],
+            "Katsina": [
+                "Bakori", "Batagarawa", "Batsari", "Baure", "Bindawa", "Charanchi", "Dandume", "Danja",
+                "Dan Musa", "Daura", "Dutsi", "Dutsin Ma", "Faskari", "Funtua", "Ingawa", "Jibia", "Kafur",
+                "Kaita", "Kankara", "Kankia", "Katsina", "Kurfi", "Kusada", "Mai'Adua", "Malumfashi",
+                "Mani", "Mashi", "Matazu", "Musawa", "Rimi", "Sabuwa", "Safana", "Sandamu", "Zango"
+            ],
+            "Kebbi": [
+                "Aleiro", "Arewa Dandi", "Argungu", "Augie", "Bagudo", "Birnin Kebbi", "Bunza", "Dandi",
+                "Fakai", "Gwandu", "Jega", "Kalgo", "Koko/Besse", "Maiyama", "Ngaski", "Sakaba", "Shanga",
+                "Suru", "Wasagu/Danko", "Yauri", "Zuru"
+            ],
+            "Sokoto": [
+                "Binji", "Bodinga", "Dange Shuni", "Gada", "Goronyo", "Gudu", "Gwadabawa", "Illela", "Isa",
+                "Kebbe", "Kware", "Rabah", "Sabon Birni", "Shagari", "Silame", "Sokoto North", "Sokoto South",
+                "Tambuwal", "Tangaza", "Tureta", "Wamako", "Wurno", "Yabo"
+            ],
+            "Zamfara": [
+                "Anka", "Bakura", "Birnin Magaji/Kiyaw", "Bukkuyum", "Bungudu", "Gummi", "Gusau", "Kaura Namoda",
+                "Maradun", "Maru", "Shinkafi", "Talata Mafara", "Tsafe", "Zurmi"
             ]
         }
 
-        for state_name, lga_list in southwest_data.items():
-            state, _ = State.objects.get_or_create(name=state_name, defaults={'is_active': True})
+        total_states = 0
+        total_lgas = 0
+
+        for state_name, lga_list in nigerian_states_data.items():
+            state, state_created = State.objects.get_or_create(name=state_name, defaults={'is_active': True})
+            total_states += 1
+            
+            state_action = "Created" if state_created else "Updated"
+            self.stdout.write(f"{state_action} State: {state.name}")
             
             for lga_name in lga_list:
-                lga, created = LGA.objects.update_or_create(
+                lga, lga_created = LGA.objects.update_or_create(
                     name=lga_name,
                     state=state,
                     defaults={'is_active': True}
                 )
-                action = "Created" if created else "Updated"
-                self.stdout.write(f"{action}: {lga.name} in {state.name}")
+                total_lgas += 1
+                action = "Created" if lga_created else "Updated"
+                self.stdout.write(f"  {action} LGA: {lga.name}")
 
-        self.stdout.write(self.style.SUCCESS("South West States and LGAs loaded or updated successfully."))
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully processed {total_states} states and {total_lgas} LGAs for Nigeria."
+            )
+        )
+        self.stdout.write(
+            self.style.SUCCESS(
+                "All Nigerian States and LGAs have been loaded or updated successfully."
+            )
+        )
