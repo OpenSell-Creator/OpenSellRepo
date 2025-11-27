960b7b5ce0ff7d6705ed6783a3dbf4887a43438b
Open-Sell <opensellmarketplace@gmail.com>
2025-11-27 09:21:59 +0100

    Update 0008_bulkemail.py

--- User/migrations/0008_bulkemail.py
+++ User/migrations/0008_bulkemail.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('User', '0007_profile_available_for_services_profile_buyer_rating_and_more'),
+        ('User', '0006_profile_business_last_verification_attempt_and_more.py'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
